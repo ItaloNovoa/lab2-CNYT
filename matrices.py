512a8330d375701c20a4039209cfd1a862c856f1
unknown <2135142@labinfo.is.escuelaing.edu.co>
2019-08-29 16:01:14 -0500

    ejemplos

--- matrices.py
+++ matrices.py
@@ -24,7 +24,9 @@ def negativaMatriz(m1):
             vector.append((m1[i][j][0]*-1,m1[i][j][1]*-1))
         matriz.append(vector)
     return matriz
-    
+
+#ejemplo
+#complejoPorMatriz((3,1),[[(1,0),(1,0),(1,0)]])
 def complejoPorMatriz(c1,m1):
     matriz=[]
     for i in range(0,len(m1)):
@@ -34,6 +36,8 @@ def complejoPorMatriz(c1,m1):
         matriz.append(vector)
     return matriz
 
+#ejemplo
+#transpuesta([[(3,2),(4,2)],[(5,1),(6,3)]])
 def transpuesta(m1):
     matriz=[]
     for i in range(0,len(m1)):
@@ -43,20 +47,41 @@ def transpuesta(m1):
         matriz.append(vector)
     return matriz
 
+#ejemplo
+#conjugada([[(3,2),(4,2)],[(5,1),(6,3)]])
 def conjugada(m1):
     matriz=[]
     for i in range(0,len(m1)):
         vector=[]
         for j in range(0,len(m1[0])):
-            vector.append((m1[i][j][0],m1[i][j][1]*-1))
+            vector.append((m1[i][j][0],m1[i][j][1]*-1))        
         matriz.append(vector)
     return matriz
 
+#ejemplo
+#adjunta([[(3,2),(4,2)],[(5,1),(6,3)]])
 def adjunta(m1):
     matriz=transpuesta(m1)
     matriz=conjugada(matriz)
     return matriz
 
+#------------------------------COMPLEMENTOS------------------------------
+
+#recibe dos tuplas con una parte real y otra imaginaria
+#EJEMPLO --> (3,-4),(-5,18)
+def producto(c1,c2):
+    pr=(c1[0]*c2[0])-(c1[1]*c2[1])
+    pi=(c1[0]*c2[1])+(c2[0]*c1[1])
+    return (pr,pi)
+
+
+def bonita(mi):
+    for i in range(len(mi)):
+        for j in range(len(mi[i])):
+            print(mi[i][j], end= " ")
+        print()
+
+#------------------------------Pruebas------------------------------    
 
 class TestUM(unittest.TestCase):
     #suma
@@ -73,9 +98,4 @@ class TestUM(unittest.TestCase):
 if __name__ =='__main__':
     unittest.main()
     
-#recibe dos tuplas con una parte real y otra imaginaria
-#EJEMPLO --> (3,-4),(-5,18)
-def producto(c1,c2):
-    pr=(c1[0]*c2[0])-(c1[1]*c2[1])
-    pi=(c1[0]*c2[1])+(c2[0]*c1[1])
-    return (pr,pi)
+
