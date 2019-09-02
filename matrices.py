557d8e305569e2c38ddda534ad7b3661326dda77
ItaloNovoa <italolo97@hotmail.com>
2019-09-01 23:32:20 -0500

    falta producto tensor

--- matrices.py
+++ matrices.py
@@ -1,6 +1,13 @@
 import math
 import unittest
 
+"""
+C ->  Numeros Complejos
+m1,m2 E C^(n*m) ^ m1,m2 E C^(n)
+c,c1,c2 E C
+"""
+
+
 #ejemplo
 #sumaMatrices([[[3,2],[4,-2]],[[1,2],[3,2]]],[[[5,-6],[7,2]],[[2,5],[-12,-9]]])
 def sumaMatrices(m1,m2):
@@ -65,6 +72,92 @@ def adjunta(m1):
     matriz=conjugada(matriz)
     return matriz
 
+
+#Ejemplos
+#m1=[[(1,0),(2,0),(-3,0)],[(4,0),(0,0),(-2,0)]]
+#m2=[[(3,0),(1,0)],[(2,0),(4,0)],[(-1,0),(5,0)]]
+#m2=[[(1,0)],[(1,-2)],[(0,-1)]]
+#m1=[[(3,0),(1,2),(0,-1)],[(2,-1),(0,0),(1,0)],[(0,0),(4,3),(0,1)]]
+def multiplicacionDeMatrices(m1,m2):
+    matriz=[]
+    for i in range(len(m1)):
+        vector=[]        
+        for j in range(len(m2[i])):
+            suma=(0,0)
+            for k in range (len(m2)):                
+                if(len(m1[i])!=len(m2)):
+                    return "mala entrada"
+                else:
+                    suma=sumas(suma,producto(m1[i][k],m2[k][j]))
+            vector.append(suma)
+        matriz.append(vector)
+
+    return matriz
+
+#Ejemplo
+#m1=[[(2.8,5.8),(3.4,-8.4)]]
+#Norma(m1)
+def Norma(m1):
+    resultado=(0,0)
+    for i in range(len(m1)):
+        for j in range(len(m1[i])):
+            resultado=sumas(producto(m1[i][j],conjugado(m1[i][j])),resultado)
+    return resultado[0]**0.5
+
+#EJEMPLO
+#m1=[[(2.8,5.8),(3.4,-8.4)]]
+#distancia(m1,m1)
+def distancia(m1,m2):
+    resultado=(0,0)
+    if(len(m1)-len(m2)!=0):
+        return "las dimensiones deben ser iguales"
+    else:
+        for i in range(len(m1)):
+            for j in range(len(m1[i])):
+                resultado=sumas(producto(resta(m1[i][j],m2[i][j]),resta(conjugado(m1[i][j]),conjugado(m2[i][j]))),resultado)
+        return resultado[0]**0.5
+
+#EJEMPLO
+#m1=[[(1,0),(0,0),(0,0)],[(0,0),(0,0),(1,0)],[(0,0),(1,0),(0,0)]]
+#Unitario(m1)
+def Unitaria(m1):
+    MatrizIdentidad=[[(1,0) if j == i else (0,0) for j in range(len(m1))] for i in range(len(m1))]
+    for i in range(len(m1)):
+        if(len(m1)!=len(m1[i])):
+            return "la matriz debe ser de mXm"        
+
+    if(multiplicacionDeMatrices(m1,adjunta(m1))==MatrizIdentidad):
+        return True
+    else:
+        return False
+    
+    
+
+#Ejemplo
+#m1=[[(5,0),(4,5),(6,-16)],[(4,-5),(13,0),(7,0)],[(6,16),(7,0),(-2.1,0)]]
+#Hermitian(m1):    
+def Hermitian(m1):
+    if(len(m1[0])!=len(m1[1])):
+        return "la matriz debe ser de mXm"
+    elif(m1==adjunta(m1)):
+        return True
+    else:
+        return False
+# m=[[(2,0),(3,0)]]
+# m1=[[(4,0),(6,0),(3,0)]]
+def ProductoTensor(m1,m2):
+    LaMegaMatriz=[]
+    #[[(0,0)]*(len(m1[0])*len(m2[0]))]*(len(m1)*len(m2))
+    for i in range(len(m1)):
+        Matrizes=[]
+        for j in range(len(m1[i])):
+            Matrizes.append(complejoPorMatriz(m1[i][j],m2))
+        LaMegaMatriz.append(Matrizes)
+    return (LaMegaMatriz)
+                        
+
+            
+
 #------------------------------COMPLEMENTOS------------------------------
 
 #recibe dos tuplas con una parte real y otra imaginaria
@@ -78,8 +171,33 @@ def producto(c1,c2):
 def bonita(mi):
     for i in range(len(mi)):
         for j in range(len(mi[i])):
-            print(mi[i][j], end= " ")
+            if((mi[i][j][1])>0):
+                print(str(mi[i][j][0])+"+"+str(mi[i][j][1])+"i", end= " ")
+            elif((mi[i][j][1])==0):
+                print(str(mi[i][j][0]), end= " ")
+            else:
+                print(str(mi[i][j][0])+str(mi[i][j][1])+"i", end= " ")
+                
         print()
+        
+#recibe dos tuplas con una parte real y otra imaginaria
+#EJEMPLO --> (3,-4),(-5,18)
+def sumas(c1,c2):
+    preal=c1[0]+c2[0]
+    pimaginaria=c1[1]+c2[1]
+    return (preal,pimaginaria)
+
+#recibe una tupla con una parte real y otra imaginaria
+#EJEMPLO --> (3,-4)
+def conjugado(c1):
+    return (c1[0],(c1[1]*-1))
+
+#recibe dos tuplas con una parte real y otra imaginaria
+#EJEMPLO --> (3,-4),(-5,18)
+def resta(c1,c2):
+    preal=c1[0]-c2[0]
+    pimaginaria=c1[1]-c2[1]
+    return (preal,pimaginaria)
 
 #------------------------------Pruebas------------------------------    
 
@@ -90,9 +208,40 @@ class TestUM(unittest.TestCase):
     #Negativa de Matriz   
     def test_caso_NegativaDeMatriz_1(self):
         self.assertEqual([[(-5, 6), (-7, -2)], [(-2, -5), (12, 9)]],negativaMatriz([[[5,-6],[7,2]],[[2,5],[-12,-9]]]))
-    """def test_caso_producto_2(self):
-        self.assertEqual((1,21),producto((1,4),(5,1)))"""
-   
+    #complejoPorMatriz
+    def test_caso_complejoPorMatriz_1(self):
+        self.assertEqual([[(3, 1), (3, 1), (3, 1)]],complejoPorMatriz((3,1),[[(1,0),(1,0),(1,0)]]))
+    #transpuesta
+    def test_caso_transpuesta(self):
+        self.assertEqual([[(3, 2), (5, 1)], [(4, 2), (6, 3)]],transpuesta([[(3,2),(4,2)],[(5,1),(6,3)]]))
+    #conjugada
+    def test_caso_Conjugada(self):
+        self.assertEqual([[(3, -2), (4, -2)], [(5, -1), (6, -3)]],conjugada([[(3,2),(4,2)],[(5,1),(6,3)]]))
+    #adjunta([[(3,2),(4,2)],[(5,1),(6,3)]])
+    def test_caso_adjunta(self):
+        self.assertEqual([[(3, -2), (5, -1)], [(4, -2), (6, -3)]],adjunta([[(3,2),(4,2)],[(5,1),(6,3)]]))
+    #multiplicacionDeMatrices
+    def test_caso_multiplicacionDeMatrices(self):
+        m2=[[(1,0)],[(1,-2)],[(0,-1)]]
+        m1=[[(3,0),(1,2),(0,-1)],[(2,-1),(0,0),(1,0)],[(0,0),(4,3),(0,1)]]
+        self.assertEqual([[(7, 0)], [(2, -2)], [(11, -5)]],multiplicacionDeMatrices(m1,m2))
+    #Norma
+    def test_caso_Norma(self):
+        m1=[[(2.8,5.8),(3.4,-8.4)]]        
+        self.assertEqual(11.117553687749837,Norma(m1))
+    #Distancia
+    def test_caso_Distancia(self):
+        m1=[[(2.8,5.8),(3.4,-8.4)]]        
+        self.assertEqual(0.0,distancia(m1,m1))
+    #Unitaria
+    def test_caso_Unitaria(self):
+        m1=[[(1,0),(0,0),(0,0)],[(0,0),(0,0),(1,0)],[(0,0),(1,0),(0,0)]]       
+        self.assertEqual(True,Unitaria(m1))
+    #Hermitian    
+    def test_caso_Hermitian(self):
+        m1=[[(5,0),(4,5),(6,-16)],[(4,-5),(13,0),(7,0)],[(6,16),(7,0),(-2.1,0)]]        
+        self.assertEqual(True,Hermitian(m1))
+        
         
 
 if __name__ =='__main__':
